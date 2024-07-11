fec9c9067b7735250d0675240a4ec1f492cf233d
Johnson Huang <johnsonhuang@xaregroup.com>
2024-07-11 17:44:05 +0800

    modify title and seo settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,14 +14,14 @@ const fontNotoSans = Noto_Sans_TC({
 
 export const metadata: Metadata = {
   title: {
-    default: '24點大師',
-    template: '24點大師 | %s',
+    default: '24點',
+    template: '24點 | %s',
   },
   description:
     '歡迎來到24點數學遊戲！這是一款充滿挑戰和樂趣的益智遊戲，考驗你的數學運算能力和邏輯思維',
-  keywords: ['24點', 'math24', '24點大師'],
+  keywords: ['24點', 'math24', '24點大師', '數學24點'],
   openGraph: {
-    title: '24點大師',
+    title: '24點',
     description:
       '歡迎來到24點數學遊戲！這是一款充滿挑戰和樂趣的益智遊戲，考驗你的數學運算能力和邏輯思維',
     images: [
