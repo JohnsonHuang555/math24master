554e71e5192e7610100723592a602c531ca8d91e
Johnson Huang <johnsonhuang@xaregroup.com>
2026-04-25 19:23:22 +0800

    fix firebase admin privateKey

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -6,7 +6,7 @@ if (!getApps().length) {
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
   });
 }
